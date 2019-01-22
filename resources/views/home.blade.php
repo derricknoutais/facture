d4147b13b95e4fa0d9400721eadeb4dc1fc02afc
Derrick Noutais <derricknoutais@gmail.com>
2019-01-22 15:43:11 +0100

    Added Middleware

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="jumbotron jumbotron-fluid">
-    <div class="container">
+    <div class="container text-center">
         <div class="row text-center mb-5">
             <div class="col">
                 <img src="/img/logo.png" width="500vw">
@@ -11,11 +11,9 @@
         <h1 class="display-5 text-center">Créer un Compte Pour Gérer vos Factures</h1>
 
 
-        <p class="lead">Jumbo helper text</p>
-        <hr class="my-2">
-        <p>More info</p>
+        <p class="lead text">Un Agent Validera votre demande dans les plus Brefs Délais</p>
         <p class="lead">
-            <a class="btn btn-primary btn-lg" href="Jumbo action link" role="button">Jumbo action name</a>
+            <a class="btn btn-primary btn-lg" href="/register" role="button">Créer un Compte</a>
         </p>
     </div>
 </div>
