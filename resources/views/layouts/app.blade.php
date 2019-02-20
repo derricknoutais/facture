8915b031981be2c96d21678872ab7c17047838fb
Derrick Noutais <derricknoutais@gmail.com>
2019-02-20 23:26:38 +0100

    Added Icons

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,7 +41,7 @@
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/accueil') }}">
-                    <img class="mt-1" src="/img/logo.png" width="80vw">
+                    <img class="mt-1" src="/img/logo-only.png" width="80vw">
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
