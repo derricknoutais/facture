a0a410aa33cc2c4d5a4e89b4957217a7fb227f16
Derrick Noutais <derricknoutais@gmail.com>
2019-08-26 09:09:50 +0100

    Added X-Socket-Id

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -46,6 +46,29 @@ if (token) {
 
 import Echo from 'laravel-echo';
 
+Vue.http.interceptors.push(function (request, next) {
+
+    //some api's dont like the X-CSFR-token or Pusher token.. remove it..
+    const removeAuthHeaders = request.url.includes("openiban.com");
+
+    request.headers['Access-Control-Allow-Origin'] = '*';
+
+    if (removeAuthHeaders) {
+        request.headers.delete('Access-Control-Request-Headers')
+        request.headers.delete('X-Socket-ID');
+
+    }
+    else {
+        request.headers.set('X-CSRF-TOKEN', Laravel.csrfToken);
+    }
+
+
+
+    next(function (response) {
+
+    });
+});
+
 window.Pusher = require('pusher-js');
 
 window.Echo = new Echo({
